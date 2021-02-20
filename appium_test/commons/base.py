dbf7f4325797b57b1ad3e66b13af3db6a0edcd00
unknown <zhangqin8909@ppc202006087374.ad.zto.com>
2021-02-20 17:04:09 +0800

    appium第一次作业

---
+++ appium_test/commons/base.py
@@ -0,0 +1,40 @@
+from appium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+    def __init__(self, driver: WebDriver = None):
+        if driver is None:
+            caps = {}
+            caps["platformName"] = "android"
+            caps["appPackage"] = "com.tencent.wework"
+            caps["appActivity"] = ".launch.LaunchSplashActivity"
+            caps["noReset"] = "true"
+
+            self._driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+            self._driver.implicitly_wait(5)
+        else:
+            self._driver = driver
+
+    def find_ele(self, by, locator=None):
+        if locator is None:
+            result = self._driver.find_element(*by)
+        else:
+            result = self._driver.find_element(by, locator)
+
+        return result
+
+    def find_and_sendkey(self, by, text):
+        self.find_ele(by).send_keys(text)
+
+    def find_and_click(self, by):
+        self.find_ele(by).click()
+
+    def find_and_gettext(self, by):
+        self.find_ele(by).text
+
+    def close(self):
+        self._driver.quit()
+
---
+++ appium_test/test_cases/first_hm/test_case_demo.py
@@ -0,0 +1,31 @@
+from appium.webdriver.common.mobileby import MobileBy
+
+from appium_test.commons.base import BasePage
+
+
+class TestDemo:
+    def test_one(self):
+        print(111)
+
+    def setup(self):
+        self.base = BasePage()
+
+    def test_add_member(self):
+        self.base.find_and_click((MobileBy.XPATH, "//*[@text='通讯录']"))
+        self.base.find_and_click((MobileBy.ANDROID_UIAUTOMATOR,
+                                  'new UiScrollable(new UiSelector().'
+                                  'scrollable(true).instance(0)).'
+                                  'scrollIntoView(new UiSelector().'
+                                  'text("添加成员").instance(0));'))
+        self.base.find_and_click((MobileBy.XPATH, "//*[@text='手动输入添加']"))
+        self.base.find_and_sendkey((MobileBy.XPATH, "//*[contains(@text,'姓名')]/../android.widget.EditText"), "zhangqin2")
+        self.base.find_and_sendkey((MobileBy.XPATH, "//*[contains(@text,'帐号')]/../android.widget.EditText"), "zhangqin2")
+        self.base.find_and_click((MobileBy.ID, "com.tencent.wework:id/eso"))
+        self.base.find_and_click((MobileBy.XPATH, "//*[@text='女']"))
+        self.base.find_and_sendkey((MobileBy.ID, "com.tencent.wework:id/fwi"), "18721901111")
+        self.base.find_and_click((MobileBy.ID, "com.tencent.wework:id/aj_"))
+        toast = self.base.find_and_gettext((MobileBy.XPATH, '//*[@class="android.widget.Toast"]'))
+        assert toast == '添加成功'
+
+    def teardown(self):
+        self.base.close()
